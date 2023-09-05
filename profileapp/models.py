926f7c1ee79864e8762641b25b31d420ff33e868
Minji6 <pullzee6@gmail.com>
2023-09-05 17:23:21 +0900

    django course 32 commit

--- profileapp/models.py
+++ profileapp/models.py
@@ -6,8 +6,6 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
-    image = models.ImageField(upload_to='profile/', null=True)
-
-    nickname = models.CharField(max_length=20, unique=True, null=True)
-
-    message = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to='profile/', null=True) # 이미지 필드 생성
+    nickname = models.CharField(max_length=20, unique=True, null=True) # 닉네임은 1계정 1닉네임
+    message = models.CharField(max_length=100, null=True) # 메세지 필드 생성
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -15,9 +17,19 @@ class ProfileCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
-    def form_valid(self, form):
-        temp_profile = form.save(commit=False)
+    def form_valid(self, form): #form_vaild 함수를 통해서 로직을 수행하고 성공적으로 마치면 알아서 success_url에 정의한 페이지로 이동한다.
+        temp_profile = form.save(commit=False) # 임시저장
         temp_profile.user = self.request.user
         temp_profile.save()
-        return super().form_valid(form)
+        return super().form_valid(form) #슈퍼클래스, 즉 부모인 CreateView를 상속
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
+
 
