fea4593ad236d422eba66ff6f6f6b672eb0404e3
Minji6 <pullzee6@gmail.com>
2023-09-10 03:08:32 +0900

    django course 33 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -14,7 +14,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form): #form_vaild 함수를 통해서 로직을 수행하고 성공적으로 마치면 알아서 success_url에 정의한 페이지로 이동한다.
@@ -23,13 +22,19 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form) #슈퍼클래스, 즉 부모인 CreateView를 상속
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk}) #self.object가 가르키는 것은 Profile이다.
+
+
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk}) #self.object가 가르키는 것은 Profile이다.
 
 
