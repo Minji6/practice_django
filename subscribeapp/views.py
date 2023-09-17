c5b7267286c1ca21e43213802c0a439e879db0cb
Minji6 <pullzee6@gmail.com>
2023-09-17 20:22:50 +0900

    django course 44 commit

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import RedirectView, ListView
@@ -10,29 +8,33 @@ from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
+
+# Create your views here.
 @method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class SubscriptionView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
-        return reverse('projectapp:detail', kwargs={'pk':self.request.GET.get('project_pk')})
+        return reverse('projectapp:detail', kwargs={'pk': self.request.GET.get('project_pk')})
 
     def get(self, request, *args, **kwargs):
         project = get_object_or_404(Project, pk=self.request.GET.get('project_pk'))
         user = self.request.user
+
         subscription = Subscription.objects.filter(user=user, project=project)
 
         if subscription.exists():
             subscription.delete()
         else:
             Subscription(user=user, project=project).save()
-        return super(SubscriptionView, self).get(request, *args, **kwargs)
 
+        return super(SubscriptionView, self).get(request, *args, **kwargs)
 
 @method_decorator(login_required, 'get')
 class SubscriptionListView(ListView):
     model = Article
     context_object_name = 'article_list'
-    template_name = 'subscripbeapp/list.html'
+    template_name = 'subscribeapp/list.html'
     paginate_by = 5
 
     def get_queryset(self):
