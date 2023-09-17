d7489fe1a03e6aa6e329ab9e889a515493ab7283
Minji6 <pullzee6@gmail.com>
2023-09-17 20:55:33 +0900

    django course 45 commit

--- projectapp/models.py
+++ projectapp/models.py
@@ -7,3 +7,7 @@ class Project(models.Model):
     description = models.CharField(max_length=200, null=True)
 
     create_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # 따옴표 앞에 f를 붙이면 직접 변수를 출력할 수 있다.
+        return f'{self.pk} : {self.title}'
